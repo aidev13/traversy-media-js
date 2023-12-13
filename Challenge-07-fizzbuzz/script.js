76ed9b9492981232f484c0476e3a08b8237f4035
David Liebherr <ambition.aidev@gmail.com>
2023-12-13 16:19:32 -0600

    Used while loop to make fizzbuzz

--- Challenge-07-fizzbuzz/script.js
+++ Challenge-07-fizzbuzz/script.js
@@ -1,15 +1,15 @@
 //FizzBuzz
-for(let i =1;i<=100;i++) {
-   if(i % 15 === 0) {
-      console.log('FizzBuzz');
-   } else if (i % 5 === 0){
-      console.log('Buzz')
-   } else if(i % 3 === 0){
-      console.log('Fizz');
-   } else {
-      console.log(i);
-   }
-};
+// for(let i =1;i<=100;i++) {
+//    if(i % 15 === 0) {
+//       console.log('FizzBuzz');
+//    } else if (i % 5 === 0){
+//       console.log('Buzz')
+//    } else if(i % 3 === 0){
+//       console.log('Fizz');
+//    } else {
+//       console.log(i);
+//    }
+// };
 
 
 
@@ -19,3 +19,27 @@ for(let i =1;i<=100;i++) {
 // let odd = y % 1
 
 // console.log(odd);
+
+// let i = 1;
+
+// while ( i <= 100) {
+//    if(i % 15 === 0) {
+//             console.log('FizzBuzz');
+//          } else if (i % 5 === 0){
+//             console.log('Buzz')
+//          } else if(i % 3 === 0){
+//             console.log('Fizz');
+//          } else {
+//             console.log(i);
+//          }
+//    i++
+// }
+
+// console.log(i);
+
+// let x = 1
+
+// while (x <= 10) {
+//    console.log(x);
+//    x++
+// }
