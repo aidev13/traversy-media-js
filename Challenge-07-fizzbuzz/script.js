4a92d626219bd12ba22572bbd89dda7796bdb522
David Liebherr <ambition.aidev@gmail.com>
2023-12-12 20:45:23 -0600

    put fizzbuzz into one for-loop

--- Challenge-07-fizzbuzz/script.js
+++ Challenge-07-fizzbuzz/script.js
@@ -1,44 +1,21 @@
-// for(let i = 1; i <= 100; i++) {
-
-//    console.log(i);
-// }
-
-//Fizz
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log("Fizz");
-  } else {
-    console.log(i);
-  }
-}
-
-//Bizz
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 5 === 0) {
-//     console.log('Bizz');
-//   } else {
-//     console.log(i);
-//   }
-// }
-
-//FizzBizz
-// for(let i = 1; i <= 100; i++) {
-//  if(i % 15 === 0) {
-//    console.log("FizzBizz");
-//  } else {
-//     console.log(i);
-//  };
-// }
-
-
-// for (let i = 1; i <= 100; i++) {
-//    i % 3 === 0 ? console.log('Fizz'): console.log(i);
-// };
-
-// for (let i = 1; i <= 100; i++) {
-//    i % 5 === 0 ? console.log('Bizz'): console.log(i);
-// };
-
-for(let i = 1; i <= 100; i++) {
-   i % 15 === 0 ? console.log('FizzBizz'): console.log(i);
+//FizzBuzz
+for(let i =1;i<=100;i++) {
+   if(i % 15 === 0) {
+      console.log('FizzBuzz');
+   } else if (i % 5 === 0){
+      console.log('Buzz')
+   } else if(i % 3 === 0){
+      console.log('Fizz');
+   } else {
+      console.log(i);
+   }
 };
+
+
+
+
+// let y = 6
+
+// let odd = y % 1
+
+// console.log(odd);
