ef2e86e342fe3aca4dc50fec0438af142a2ade72
David Liebherr <ambition.aidev@gmail.com>
2023-12-12 18:22:22 -0600

    Fizzbizz challange

---
+++ Challenge-07-fizzbuzz/script.js
@@ -0,0 +1,31 @@
+// for(let i = 1; i <= 100; i++) {
+
+//    console.log(i);
+// }
+
+//Fizz
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3) {
+//     console.log(i);
+//   } else {
+//     console.log("Fizz");
+//   }
+// }
+
+//Bizz
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5) {
+//     console.log(i);
+//   } else {
+//     console.log("Bizz");
+//   }
+// }
+
+//FizzBizz
+for(let i = 1; i <= 100; i++) {
+ if(i % 15) {
+   console.log(i);
+ } else {
+    console.log('FizzBizz');
+ };
+}
