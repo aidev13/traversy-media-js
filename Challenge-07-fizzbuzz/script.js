a71642a408e18c0bb166f6790224e062d6c66e6d
David Liebherr <ambition.aidev@gmail.com>
2023-12-12 18:37:33 -0600

    converted statments to ternary operators

--- Challenge-07-fizzbuzz/script.js
+++ Challenge-07-fizzbuzz/script.js
@@ -4,28 +4,41 @@
 // }
 
 //Fizz
-// for (let i = 1; i <= 100; i++) {
-//   if (i % 3) {
-//     console.log(i);
-//   } else {
-//     console.log("Fizz");
-//   }
-// }
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {
+    console.log(i);
+  }
+}
 
 //Bizz
 // for (let i = 1; i <= 100; i++) {
-//   if (i % 5) {
-//     console.log(i);
+//   if (i % 5 === 0) {
+//     console.log('Bizz');
 //   } else {
-//     console.log("Bizz");
+//     console.log(i);
 //   }
 // }
 
 //FizzBizz
+// for(let i = 1; i <= 100; i++) {
+//  if(i % 15 === 0) {
+//    console.log("FizzBizz");
+//  } else {
+//     console.log(i);
+//  };
+// }
+
+
+// for (let i = 1; i <= 100; i++) {
+//    i % 3 === 0 ? console.log('Fizz'): console.log(i);
+// };
+
+// for (let i = 1; i <= 100; i++) {
+//    i % 5 === 0 ? console.log('Bizz'): console.log(i);
+// };
+
 for(let i = 1; i <= 100; i++) {
- if(i % 15) {
-   console.log(i);
- } else {
-    console.log('FizzBizz');
- };
-}
+   i % 15 === 0 ? console.log('FizzBizz'): console.log(i);
+};
